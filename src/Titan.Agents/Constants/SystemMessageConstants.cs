9473ee343d2031746f47e38f09a25eec0d224d06
Kyle Unger <kylemunger99@gmail.com>
2023-07-20 17:53:30 -0400

    fix dir

---
+++ src/Titan.Agents/Constants/SystemMessageConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Titan.Agents.Constants
+{
+    public static class SystemMessageConstants
+    {
+        public const string SYSTEM_CONTROLLER = "";
+    }
+}
